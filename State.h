b919dac5e74ee31e8f7b85b8c9ce8180455f02bc
PirateCody <piratecody16@gmail.com>
2019-12-15 03:22:28 -0600

    Update State.h
    Various getters and comments

--- State.h
+++ State.h
@@ -8,9 +8,29 @@ using namespace std;
 
 class State {
 public:
+
+	/*Returns the name of the State*/
 	string getName() const;
+
+	/*Returns the value of the State Modifier*/
+	double getStateModifier() const;
+
+	/*Returns the number of districts*/
+	unsigned int getNumDistricts() const;
+
+	/*Returns the district with the specified key*/
+	District* getDistrict(string key);
 private:
+	/*Value of any statewide modifier*/
+	double stateModifier;
+
+	/*Number of Districts*/
 	unsigned int numDistricts;
+
+	/*Map containing pointers to the districts within a state*/
 	map<string, District*> districts;
+
+	/*State Name*/
+	string name;
 };
 #endif

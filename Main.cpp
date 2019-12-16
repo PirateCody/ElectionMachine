23d02fc2945f7372973a0f572ea05245c55e5994
PirateCody <piratecody16@gmail.com>
2019-12-16 04:07:36 -0600

    Baseline for CLI
    Added the baseline for CLI

--- Main.cpp
+++ Main.cpp
@@ -9,9 +9,34 @@
 #include <string>
 using namespace std;
 
+void mainMenu() {
+	cout << "******************************************\n";
+	cout << "*******Welcome to Election Machine!*******\n";
+	cout << "******************************************\n\n";
+}
 
+void processInput(string input, bool* active) {
+	if (input == "exit") {
+		*active = false;
+		cout << "Exiting\n";
+	}
+	else {
+		cout << "Unknown command" << endl;
+	}
+}
 
 int main() {
+
+	mainMenu();
+	bool active = true;
+
+	string input = "";
+
+	while (active) {
+		cout << "Election Machine>"; getline(cin, input);
+		cin.ignore();
+		processInput(input, &active);
+	}
 
 
 	system("pause");
